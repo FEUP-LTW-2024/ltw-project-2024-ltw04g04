777c541bbfbe0c7c978e2dec270dd6099d4c457c
Sofia <sofiaagoncalves25@gmail.com>
2024-04-21 18:56:05 +0100

    Base of item page

--- pages/item.php
+++ pages/item.php
@@ -10,36 +10,20 @@ generateNavigationMenu($categories);
     <main>
         <section id="item">
             <div id="itemImg"><img src="imgs/itemTemplate.png" alt="Image of item"></div>   <!-- CHANGE -->
+            
+            <div id="containers">
             <div id="itemContainer">
-                <form action="/updateProfile" method="post" class =  "editForm">
-                    <h2>Product Name</h2>       <!-- CHANGE -->
-                    
-
-                    <button type="button" id="addItemToCart">Add to shopping cart</button>
-                </form>
+                <h2>Product Name</h2>       <!-- CHANGE -->
+                <button type="button" id="addItemToCart">Add to shopping cart</button>
             </div>
 
-            <!-- Follow button -->
-            
+            <div id="itemContainer">
+                <h3>Seller Name</h3>       <!-- CHANGE -->
+                <button type="button" id="accountSeller"> > </button>
+            </div>
+            </div>
 
         </section>
-
-        <!-- Articles Section -->
-        <section id = "articles">
-            <!-- Article 1 -->
-            <article class ="articleItem">
-                <!-- Image of article 1 -->
-                <!-- You need to replace 'article1.jpg' with actual path of your image-->
-                <img src ="imgs/article1.jpg"class ="articleImage"alt ="Article 1 Image">
-
-                <!-- Description of article 1-->
-                <!--<p> Description about this article.</p>-->
-                
-            </article>
-
-            <!-- Add more articles as needed -->
-
-        </section> 
     </main> 
 </body>
 </html>
