464e7b0a2ad4798aba53565cdec6507f64ea345e
Sofia <sofiaagoncalves25@gmail.com>
2024-04-21 20:12:32 +0100

    Seller container of item page

--- pages/item.php
+++ pages/item.php
@@ -14,10 +14,16 @@ generateNavigationMenu($categories);
             <div id="containers">
             <div id="itemContainer">
                 <h2>Product Name</h2>       <!-- CHANGE -->
+                <p> price $ </p>       <!-- CHANGE -->
                 <button type="button" id="addItemToCart">Add to shopping cart</button>
+
+                <h3>Product details</h3>
+                <p> Category: </p>       <!-- CHANGE -->
+                <!-- ADD THE OTHER CHARACTERISTICS -->
             </div>
 
-            <div id="itemContainer">
+            <div id="sellerContainer">
+                <div id="sellerImg"><img src="imgs/user-icon.png" alt="Image of icon account"></div>
                 <h3>Seller Name</h3>       <!-- CHANGE -->
                 <button type="button" id="accountSeller"> > </button>
             </div>
