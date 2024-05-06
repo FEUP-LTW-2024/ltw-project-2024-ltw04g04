db7edda30ee1c104f9010f06ff19b93c432dabd5
Sofia <sofiaagoncalves25@gmail.com>
2024-05-06 16:25:33 +0100

    Addition of search.php and template

--- actions/action_search.php
+++ actions/action_search.php
@@ -1,6 +1,5 @@
 <?php
     declare(strict_types = 1);
-    //require_once(__DIR__ . '/../utils/session.php');
     require_once(__DIR__ . '/../database/get_database.php');
     require_once(__DIR__ . '/../database/item.class.php');
 
@@ -12,13 +11,8 @@
         $search = '%'.$search.'%';
         $results = Item::getItemWithName($db, $search);
 
-        if (!empty($results)) {
-            foreach ($results as $item) {
-                echo "Nome do item: " . $item->name . "<br>";
-                // Você pode adicionar mais informações do item conforme necessário
-            }
-        } else {
-            echo "Nenhum resultado encontrado.";
-        }
+        $query_string = http_build_query(['results' => $results]);
+        header("Location: ../pages/search.php?" . $query_string); 
+        exit();
     }
 ?>
---
+++ templates/search.tpl.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1); ?>
+
+<?php function drawSearchItems(array $items) { ?>
+    <body>
+        <main>
+            
+        </main>
+    </body>
+
+<?php } ?>
